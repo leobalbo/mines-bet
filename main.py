5b1e970d89186b268c3d5db238b2cf4a7ab8a1f7
leobalbo <leobalbo7@gmail.com>
2023-05-02 03:08:02 -0300

    gerar estrelas random

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 from termcolor import colored
 import telebot
-from random import randint
+# from random import randint, random
+import random
 from datetime import datetime, timedelta
 import time
 import pytz
@@ -17,43 +18,18 @@ bot = telebot.TeleBot(telegram_token,parse_mode='HTML')
 #  CONFIGURAR AQUI #
 ####################
 
-tempo_possivel_msg = randint(35,40) # Tempo em segundos primeiro valor é o minimo, segundo valor é o maximo. (Ex: Vai gerar um numero entre 45 e 70)
+tempo_possivel_msg = random.randint(35,40) # Tempo em segundos primeiro valor é o minimo, segundo valor é o maximo. (Ex: Vai gerar um numero entre 45 e 70)
 tempo_espera = 4 # Tempo que o bot vai enviar a mensagem de green.
 tempo_proxima_msg = 120 # Em segundos para enviar possivel sinal depois de dar green.
+
+# Definir o número mínimo e máximo de estrelas
+min_stars = 3
+max_stars = 4
+
 link_afiliado = "https://bit.ly/b1betminesb1bet"
 link_jogo = "https://www.b1.bet/#/game/casino?st=Mines&p=0&t=1000&g=spribe-1725-mines&f=false"
 link_jogo = "https://bit.ly/b1betminesb1bet"
 mensagem_possivel = '🌟Possíveis entradas detectadas\n\nSINAIS APENAS PARA O SITE DA B1BET\n <a href="'+link_afiliado+'">🔗 Cadastre-se aqui</a>\n\n 💰 Banca recomendada, acima de R$20,00'
-
-quantia_mensagem = 25 # Quantia de mensagem abaixo -1 (Ex: deu 26 linhas você tira 1 linha e da 25 não apagar a linha)
-lista_mensagem = [
-                    "🟦🟦🟦⭐🟦\n🟦⭐🟦🟦🟦\n🟦🟦🟦🟦🟦\n🟦🟦⭐🟦🟦\n🟦⭐🟦🟦🟦",
-                    "🟦⭐🟦🟦🟦\n🟦🟦🟦🟦🟦\n🟦🟦🟦🟦🟦\n🟦⭐⭐🟦🟦\n⭐🟦🟦🟦🟦",
-                    "🟦🟦🟦🟦🟦\n🟦🟦🟦🟦🟦\n⭐🟦🟦🟦⭐\n🟦🟦🟦🟦🟦\n🟦🟦🟦⭐🟦",
-                    "🟦⭐🟦🟦🟦\n🟦🟦🟦🟦🟦\n🟦🟦🟦🟦🟦\n🟦⭐⭐🟦🟦\n🟦🟦🟦🟦🟦",
-                    "⭐🟦🟦🟦⭐\n🟦🟦🟦🟦🟦\n🟦🟦🟦⭐🟦\n🟦🟦🟦🟦🟦\n🟦⭐🟦🟦🟦",
-                    "🟦🟦⭐🟦🟦\n🟦🟦🟦🟦🟦\n🟦🟦🟦🟦🟦\n🟦🟦⭐🟦🟦\n🟦🟦🟦⭐🟦",
-                    "🟦🟦🟦🟦🟦\n🟦🟦🟦⭐🟦\n⭐🟦🟦🟦🟦\n🟦🟦🟦🟦🟦\n🟦⭐🟦⭐🟦",
-                    "🟦🟦🟦⭐⭐\n🟦🟦🟦🟦🟦\n🟦🟦⭐🟦🟦\n🟦🟦🟦🟦🟦\n🟦🟦🟦🟦🟦",
-                    "🟦🟦🟦🟦🟦\n⭐🟦🟦🟦🟦\n🟦🟦🟦🟦🟦\n🟦🟦⭐🟦🟦\n🟦🟦🟦🟦⭐",
-                    "🟦🟦🟦🟦🟦\n⭐🟦🟦⭐⭐\n🟦🟦🟦🟦🟦\n🟦🟦🟦🟦🟦\n🟦🟦🟦🟦🟦",
-                    "🟦🟦🟦🟦🟦\n🟦🟦🟦🟦🟦\n🟦🟦⭐🟦🟦\n🟦🟦🟦🟦🟦\n🟦🟦⭐🟦⭐",
-                    "🟦🟦🟦🟦⭐\n🟦🟦🟦🟦🟦\n⭐🟦🟦🟦🟦\n🟦⭐⭐🟦🟦\n🟦🟦🟦🟦🟦",
-                    "🟦🟦🟦🟦🟦\n🟦🟦🟦⭐🟦\n🟦🟦🟦🟦🟦\n🟦🟦⭐🟦🟦\n🟦⭐🟦🟦🟦",
-                    "⭐🟦🟦🟦🟦\n🟦🟦🟦⭐⭐\n⭐🟦🟦🟦🟦\n🟦🟦🟦🟦🟦\n🟦🟦🟦🟦🟦",
-                    "🟦🟦🟦🟦🟦\n⭐🟦🟦⭐🟦\n🟦🟦🟦🟦⭐\n🟦🟦🟦🟦🟦\n⭐🟦🟦🟦🟦",
-                    "🟦🟦🟦⭐🟦\n🟦🟦🟦🟦🟦\n🟦🟦🟦🟦🟦\n🟦🟦⭐🟦🟦\n🟦⭐🟦⭐🟦",
-                    "🟦🟦🟦🟦🟦\n🟦🟦🟦⭐🟦\n🟦🟦🟦🟦🟦\n🟦🟦🟦⭐⭐\n🟦🟦🟦🟦🟦",
-                    "🟦🟦🟦🟦🟦\n🟦⭐🟦🟦⭐\n🟦🟦🟦🟦🟦\n🟦⭐🟦🟦🟦\n🟦🟦🟦🟦⭐",
-                    "🟦⭐🟦🟦⭐\n🟦🟦🟦⭐🟦\n🟦🟦🟦🟦🟦\n⭐🟦🟦🟦🟦\n🟦🟦🟦🟦🟦",
-                    "🟦⭐🟦🟦🟦\n🟦🟦🟦🟦🟦\n🟦🟦🟦🟦🟦\n⭐🟦⭐🟦🟦\n🟦🟦🟦🟦⭐",
-                    "🟦🟦🟦🟦🟦\n🟦🟦🟦🟦🟦\n🟦🟦🟦🟦⭐\n🟦⭐🟦⭐🟦\n🟦🟦🟦⭐🟦",
-                    "🟦🟦🟦⭐🟦\n🟦🟦🟦⭐🟦\n🟦🟦🟦⭐🟦\n🟦🟦🟦🟦🟦\n⭐🟦🟦🟦🟦",
-                    "🟦⭐🟦🟦🟦\n🟦🟦🟦🟦🟦\n🟦🟦🟦🟦⭐\n🟦🟦🟦⭐⭐\n🟦🟦🟦🟦🟦",
-                    "🟦🟦🟦🟦⭐\n🟦🟦🟦⭐🟦\n🟦🟦🟦🟦🟦\n⭐🟦🟦🟦🟦\n🟦🟦🟦🟦🟦",
-                    "🟦🟦🟦⭐🟦\n🟦🟦🟦🟦🟦\n🟦🟦🟦⭐🟦\n🟦🟦🟦🟦🟦\n🟦🟦⭐🟦🟦",
-                    "🟦🟦🟦🟦🟦\n🟦🟦🟦🟦🟦\n⭐🟦⭐🟦🟦\n⭐🟦🟦🟦🟦\n⭐🟦🟦🟦🟦"
-                  ]
 
 os.system('title Zety')
 print(colored('\n[Info]', 'dark_grey'), colored('Developer:', 'white'), colored('zety#7505', 'cyan'))
@@ -67,19 +43,35 @@ print(colored('''
 print(colored('\n[Info]', 'dark_grey'), colored('Bot Iniciado com sucesso', 'green'))
 
 while True:
-    bot.send_message(chat_id=chat_id, text=mensagem_possivel)
 
-    time.sleep(tempo_possivel_msg)
+  line = "🟦🟦🟦🟦🟦\n🟦🟦🟦🟦🟦\n🟦🟦🟦🟦🟦\n🟦🟦🟦🟦🟦\n🟦🟦🟦🟦🟦\n"
+  empty_spaces = [i for i in range(25) if line[i] == "🟦"]
+  num_stars = random.randint(min_stars, max_stars)
+  if len(empty_spaces) >= num_stars:
+    for i in range(num_stars):
+      index = random.choice(empty_spaces)
+      line = line[:index] + "⭐" + line[index+1:]
+      empty_spaces.remove(index)
+  else:
+    for i in range(len(empty_spaces)):
+      index = empty_spaces[i]
+      line = line[:index] + "⭐" + line[index+1:]
+    for i in range(num_stars - len(empty_spaces)):
+      index = random.choice([x for x in range(25) if x not in empty_spaces])
+      line = line[:index] + "⭐" + line[index+1:]
 
-    date = datetime.now(tz)
-    date_futur = date + timedelta(minutes=tempo_espera)
-    date_futur_format = date_futur.strftime('%H:%M')
+  bot.send_message(chat_id=chat_id, text=mensagem_possivel)
 
-    rangom_message = randint(0,quantia_mensagem)
-    bot.send_message(chat_id=chat_id, text='🔔 Entrada Cofirmada 🔔\n\n💣 Minas: 3\n🎯 Nº de tentativas: 2\n🕗 Sinal Valido até: ' + date_futur_format + '\n\n🔗 Cadastre-se aqui: <a href="'+link_jogo+'">Entrar</a> \n\n' + lista_mensagem[rangom_message])
+  time.sleep(tempo_possivel_msg)
 
-    time.sleep(tempo_espera*60)
-    date_now = datetime.now(tz)
-    date_now_format = date_futur.strftime('%H:%M')
-    bot.send_message(chat_id=chat_id, text='🔹 Sinal Finalizado 🔹\n\n🕑 Finalizado às: '+date_now_format+'\n✅✅✅GREEN✅✅✅')
-    time.sleep(tempo_proxima_msg)
+  date = datetime.now(tz)
+  date_futur = date + timedelta(minutes=tempo_espera)
+  date_futur_format = date_futur.strftime('%H:%M')
+
+  bot.send_message(chat_id=chat_id, text='🔔 Entrada Cofirmada 🔔\n\n💣 Minas: 3\n🎯 Nº de tentativas: 2\n🕗 Sinal Valido até: ' + date_futur_format + '\n\n🔗 Cadastre-se aqui: <a href="'+link_jogo+'">Entrar</a> \n\n' + line.strip())
+
+  time.sleep(tempo_espera*60)
+  date_now = datetime.now(tz)
+  date_now_format = date_futur.strftime('%H:%M')
+  bot.send_message(chat_id=chat_id, text='🔹 Sinal Finalizado 🔹\n\n🕑 Finalizado às: '+date_now_format+'\n✅✅✅GREEN✅✅✅')
+  time.sleep(tempo_proxima_msg)
